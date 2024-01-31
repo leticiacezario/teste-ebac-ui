d81bcbd51a0288963c90f52a19a8e2c14e7e2550
Leticia <leticicezario0@gmail.com>
2024-01-30 23:05:53 -0300

    Inclusão de cenários para login, pre-cadastro e produto

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,35 +2,39 @@
 
 context('Funcionalidade Login', () =>{
 
-    it('Deve fazer login com sucesso' , () => {
-        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    beforeEach(() => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/') //Acessa a url no inicio de cada teste
+    });
+    
+    afterEach(() => {
+        cy.screenshot() //Screenshot no final de cada teste
+        
+    });
+    
+    it('Deve fazer login com sucesso' , () => { //Primeiro cenário 
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.page-title').should('contain', 'Minha conta')
-
-
+        cy.get('.page-title').should('contain', 'Minha conta') //validação final, resultado esperado
 
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir usuario inválido', () =>{
-        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    it('Deve exibir uma mensagem de erro ao inserir usuario inválido', () =>{ //Segundo cenário
         cy.get('#username').type('aluno_@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error > li').should('contain', 'e-mail desconhecido')
+        cy.get('.woocommerce-error > li').should('contain', 'e-mail desconhecido') //validação final, resultado esperado
     
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
-        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{  //Terceiro cenário
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error > li').should('contain', 'incorreta')
+        cy.get('.woocommerce-error > li').should('contain', 'incorreta') //validação final, resultado esperado
     
     })
 
---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+var faker = require('faker-br'); //Para utilização do faker
+
+describe('Funcionalidade Pré-Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/') //Acessa a url no inicio de cada teste
+    });
+    
+
+    afterEach(() => {
+        cy.screenshot() //Screenshot no final de cada teste
+        
+    });
+        
+
+    it('Deve completar o pré cadastro com sucesso', () => { //Primeiro cenário
+       //Criação de variavel para utilizar variável
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+
+        cy.get('#reg_email').type(emailFaker) //utilizando faker para preencher email
+        cy.get('#reg_password').type('Teste@123!')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nomeFaker) //utilizando faker para preencher primeiro nome
+        cy.get('#account_last_name').type(sobrenomeFaker) //utilizando faker para preencher ultimo nome
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.') ///validação final, resultado esperado
+
+        
+    })
+    
+})
